7a987aab5291e39e6824733794eaa68eaf15e98c
shoujit mitra <shoujit.mitra@amdocs.com>
2023-08-29 10:27:14 -0400

    testing python

--- actions/hello_world_action_python.py
+++ actions/hello_world_action_python.py
@@ -5,7 +5,8 @@ from st2common.runners.base_action import Action
 class MyEchoAction(Action):
     def run(self, message):
         print(message)
+        return (True, message)
 
-        if "deviceIP" in message:  # == 'working':
-            return (True, message)
-        return (False, message)
+#        if "deviceIP" in message:  # == 'working':
+#            return (True, message)
+#        return (False, message)
