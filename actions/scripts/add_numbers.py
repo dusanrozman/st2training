b3d221d3106726b3574e67bc91d1e71ad75f8adf
Dusan Rozman <dule.rozman@gmail.com>
2023-09-06 16:10:00 -0400

    testing logger
    Signed-off-by: Dusan Rozman <dule.rozman@gmail.com>

--- actions/scripts/add_numbers.py
+++ actions/scripts/add_numbers.py
@@ -5,7 +5,7 @@ from st2common.runners.base_action import Action
 
 class AddNumbersAction(Action):
     def run(self, num1: int, num2):
-        print(f"num1: {num1} and num2:{num2}")
+        self.logger.info(f"num1: {num1} and num2:{num2}")
 
         if num1 and num2:
             return True, f"{num1 + num2}"
