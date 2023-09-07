c42e915f0617ca37a30633bd78b62b16bd636c17
Dusan Rozman <dule.rozman@gmail.com>
2023-09-07 15:12:36 -0400

    Testing config

--- actions/scripts/add_numbers.py
+++ actions/scripts/add_numbers.py
@@ -6,7 +6,7 @@ from st2common.runners.base_action import Action
 class AddNumbersAction(Action):
     def run(self, num1: int, num2):
         self.logger.info(f"num1: {num1} and num2:{num2}")
-
+        self.logger.info(f"Config: {self.config}")
         if num1 and num2:
             return True, f"{num1 + num2}"
         return False, "no numbers given"
