95fdca6a0db58a9f979e03d3cf5dec4f05f3ee83
Dusan Rozman <dule.rozman@gmail.com>
2023-09-13 15:05:25 -0400

    Test
    Signed-off-by: Dusan Rozman <dule.rozman@gmail.com>

--- actions/scripts/simple_script.py
+++ actions/scripts/simple_script.py
@@ -5,7 +5,7 @@ from st2common.runners.base_action import Action
 
 class SimpleScriptAction(Action):
     def run(self):
-        self.logger.info(f"This is my simple script!")
+        self.logger.info(f"This is my simple script! Cool!")
         self.action_service
         return True, "Success!"
 
