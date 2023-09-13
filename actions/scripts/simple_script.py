151be4079fd2a2e203eac65a4b2c22768ef5a76e
Dusan Rozman <dule.rozman@gmail.com>
2023-09-13 14:50:03 -0400

    updating simple script
    Signed-off-by: Dusan Rozman <dule.rozman@gmail.com>

--- actions/scripts/simple_script.py
+++ actions/scripts/simple_script.py
@@ -4,7 +4,7 @@ from st2common.runners.base_action import Action
 
 
 class SimpleScriptAction(Action):
-    def run(self, num1: int, num2):
+    def run(self):
         self.logger.info(f"This is my simple script!")
         self.action_service
         return False, "Success!"
